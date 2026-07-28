0bff61e899f7619dadef602d11b6c1cf63b8ccdd
Vyct0r <vyctorlukas50@gmail.com>
2026-07-28 20:22:20 -0300

    beecrowd

--- beecrowd/beecrowd2.cs
+++ beecrowd/beecrowd2.cs
@@ -4,6 +4,67 @@ class Bee
 {
     public static void Main(string[] args)
     {
-        
+        int a, b, c, aumA=0, aumB=0, desA=0, desB=0;
+
+        string[] partes = Console.ReadLine().Split(' ');
+
+        a = int.Parse(partes[0]);
+        b = int.Parse(partes[1]);
+        c = int.Parse(partes[2]);
+
+        if (b > a)
+        {
+            aumA = b - a;
+        }
+        else
+        {
+            desA = a - b;
+        }
+        if (c > b)
+        {
+            aumB = c - b;
+        }
+        else
+        {
+            desB = b - c;
+        }
+        if (b < a)
+        {
+            if (c >= b)
+            {
+                Console.WriteLine(":)");
+            }
+        }
+        else if (b > a)
+        {
+            if (c <= b)
+            {
+                Console.WriteLine(":(");
+            }
+        }
+        if ((b > a && c > b) && (aumB < aumA))
+        {
+           Console.WriteLine(":("); 
+        }
+        if ((b > a && c > b) && (aumB >= aumA))
+        {
+           Console.WriteLine(":)"); 
+        }
+        if ((b < a && c < b) && (desB < desA))
+        {
+            Console.WriteLine(":)");
+        }
+        if ((b < a && c < b) && (desB >= desA))
+        {
+            Console.WriteLine(":(");
+        }
+        if (b == a && c > b)
+        {
+            Console.WriteLine(":)");
+        }
+        if (b == a && c < b)
+        {
+            Console.WriteLine(":(");
+        }
     }
 }
