cd872ba6ad691d6b35d83c58d7a936308878277d
Vyct0r <vyctorlukas50@gmail.com>
2026-07-30 19:17:35 -0300

    a

--- beecrowd/beecrowd3.cs
+++ beecrowd/beecrowd3.cs
@@ -1,67 +1,13 @@
 using System;
 
-class Bee
+class bee
 {
     public static void Main(string[] args)
     {
-        string name1, name2;
-        string pi1, pi2;
-        int  soma, n1, n2;
-        bool escolheupar1, escolheupar2;
-
         int n = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < n; i++)
-        {
-          soma = 0;  
-          string [] linha = Console.ReadLine().Split(' ');
+        int hex = n.ToString("X");
 
-          name1 = linha[0];
-          pi1 = linha[1];
-          if(pi1 == "PAR")
-            {
-              escolheupar1 = true;  
-            }
-            else
-            {
-                escolheupar1 = false;
-            }        
-
-          name2 = linha[2];
-          pi2 = linha[3];
-          if (pi2 == "PAR")
-            {
-                escolheupar2 = true;
-            }
-            else
-            {
-                escolheupar2 = false;
-            }
-
-          string [] linha2 = Console.ReadLine().Split(' ');
-
-          n1 = int.Parse(linha2[0]);
-          n2 = int.Parse(linha2[1]);
-
-          soma = n1 + n2;
-
-          if(soma % 2 == 0 && escolheupar1 == true)
-            {
-                Console.WriteLine("{0}", name1);
-            }
-            else
-            {
-                Console.WriteLine("{0}", name2);
-            }
-            if(soma % 2 != 0 && escolheupar1 == false)
-            {
-                Console.Write("{0}", name1);
-            }
-            else
-            {
-                Console.WriteLine("{0}", name2);
-            }
-        }
-
+        Console.WriteLine("{0}", hex);
     }
 }
